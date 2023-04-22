43f465482ed9d3b10ce1a724b1183b2f28635902
LMaverick <91562063+LMaverick@users.noreply.github.com>
2023-04-22 16:33:39 -0300

    finalizado o catalogo de filmes

--- Carrossel.js
+++ Carrossel.js
@@ -4,12 +4,16 @@ const movie3 = document.getElementById('movie3');
 const movie4 = document.getElementById('movie4');
 const movie5 = document.getElementById('movie5');
 
+
+//=========================================================
 const info1 = document.getElementById('info1');
 const info2 = document.getElementById('info2');
 const info3 = document.getElementById('info3');
 const info4 = document.getElementById('info4');
 const info5 = document.getElementById('info5');
 
+
+//=========================================================
 const close1 = document.getElementById('close1');
 const close2 = document.getElementById('close2');
 const close3 = document.getElementById('close3');
@@ -18,7 +22,7 @@ const close5 = document.getElementById('close5');
 
 
 
-
+//=========================================================
 movie1.addEventListener('click', () => {
 	info1.classList.toggle('show');
 });
@@ -27,8 +31,20 @@ movie2.addEventListener('click', () => {
 	info2.classList.toggle('show');
 });
 
+movie3.addEventListener('click', () => {
+	info3.classList.toggle('show');
+});
+
+movie4.addEventListener('click', () => {
+	info4.classList.toggle('show');
+});
+
+movie5.addEventListener('click', () => {
+	info5.classList.toggle('show');
+});
 
 
+//=========================================================
 close1.addEventListener('click', () => {
 info1.classList.remove('show');
 });
@@ -36,3 +52,19 @@ info1.classList.remove('show');
 close2.addEventListener('click', () => {
 info2.classList.remove('show');
 });
+
+close3.addEventListener('click', () => {
+info3.classList.remove('show');
+});
+
+close4.addEventListener('click', () => {
+info4.classList.remove('show');
+});
+
+close5.addEventListener('click', () => {
+info5.classList.remove('show');
+});
+
+
+//=========================================================
+//=========================================================
