97d109a7f4de6eabd950e868e75152b92a10e563
LMaverick <91562063+LMaverick@users.noreply.github.com>
2023-04-22 16:01:13 -0300

    up

--- Carrossel.js
+++ Carrossel.js
@@ -1,11 +1,23 @@
 const movie1 = document.getElementById('movie1');
 const movie2 = document.getElementById('movie2');
+const movie3 = document.getElementById('movie3');
+const movie4 = document.getElementById('movie4');
+const movie5 = document.getElementById('movie5');
 
 const info1 = document.getElementById('info1');
 const info2 = document.getElementById('info2');
+const info3 = document.getElementById('info3');
+const info4 = document.getElementById('info4');
+const info5 = document.getElementById('info5');
 
 const close1 = document.getElementById('close1');
 const close2 = document.getElementById('close2');
+const close3 = document.getElementById('close3');
+const close4 = document.getElementById('close4');
+const close5 = document.getElementById('close5');
+
+
+
 
 movie1.addEventListener('click', () => {
 	info1.classList.toggle('show');
@@ -14,6 +26,8 @@ movie1.addEventListener('click', () => {
 movie2.addEventListener('click', () => {
 	info2.classList.toggle('show');
 });
+
+
 
 close1.addEventListener('click', () => {
 info1.classList.remove('show');
